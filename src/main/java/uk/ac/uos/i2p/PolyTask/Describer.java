77b9669b7511b91687a1726464d28ee42afeec47
Nathan Dickson <Nathan.Dickson@bt.com>
2018-11-18 14:10:15 +0000

    Sat the 18th

--- src/main/java/uk/ac/uos/i2p/PolyTask/Describer.java
+++
@@ -1,5 +0,0 @@
-package main.java.uk.ac.uos.i2p.PolyTask;
-
-interface Describer {
-	   String describe();	  
-}
--- src/main/java/uk/ac/uos/i2p/PolyTask/Processor.java
+++
@@ -1,8 +0,0 @@
-package main.java.uk.ac.uos.i2p.PolyTask;
-
-import java.io.IOException;
-import java.util.Iterator;
-
-public interface Processor {
-	void process(Iterator<String> it) throws IOException;
-}
