772f3498050f417d2af7fdc7f9421d74f5e35eef
Nathan Dickson <Nathan.Dickson@bt.com>
2018-11-26 19:07:13 +0000

    Forgot Files

---
+++ src/main/java/uk/ac/uos/i2p/JsonParser/ObjectParser.java
@@ -0,0 +1,43 @@
+package main.java.uk.ac.uos.i2p.JsonParser;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class ObjectParser {
+	private String json;
+	
+	public ObjectParser(String json) {
+		this.json = json;
+	}
+	
+	public Map<Object, Object> jsonObject() throws Exception {
+		
+		Map<Object, Object> jsonItems=new HashMap<Object, Object>();  
+		Collection<String> NameItemSplit = Arrays.asList(json.split(","));
+		
+		Iterator<String> itr = NameItemSplit.iterator();
+		while (itr.hasNext()) {
+		String item = itr.next();	
+		
+		ItemParser nathan = new ItemParser(item);
+		Map<Object, Object> newitems = nathan.jsonItem();
+		
+		  for(Map.Entry<Object, Object> m:newitems.entrySet()){  
+			   System.out.println(m.getKey()+" "+m.getValue()); 
+			   jsonItems.put(m.getKey(),m.getValue());
+			  }  
+		
+		
+		
+		
+		
+		}
+	return jsonItems;
+	
+	}
+	
+	
+}
