535db4d435a70aa476f179b20c1a7adcfbb55da0
Nathan Dickson <Nathan.Dickson@bt.com>
2018-11-26 19:07:01 +0000

    Finished Mk1 object phaser

--- src/main/java/uk/ac/uos/i2p/JsonParser/ItemParser.java
+++ src/main/java/uk/ac/uos/i2p/JsonParser/ItemParser.java
@@ -1,48 +1,80 @@
 package main.java.uk.ac.uos.i2p.JsonParser;
 
-public class ItemParser {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+
+public class ItemParser implements JsonParser {
 	private String json;
-	public ObjectCollection describeList = new ObjectCollection();
-	
+	Map<Object, Object> jsonItems=new HashMap<Object, Object>();  
 	
 	public ItemParser(String json) {
 		this.json = json;
 	}
-
 	
-	public ObjectCollection ItemProcessor() throws Exception {
-		String next = json;
+	@Override
+	public Map<Object, Object> jsonItem() throws Exception {
+        
+        // add elements to the array list
+		Collection<String> NameItemSplit = Arrays.asList(json.split(":"));
 		
-		System.out.println(next);
-		if (next.equals("null")) {
-			NullToString nullItem = new NullToString();
-			describeList.addObCollection(nullItem);
+		Iterator<String> itr = NameItemSplit.iterator();
+		String name = itr.next();
+		String item = itr.next();
+		//System.out.println(item);
+		String FirstChar = item.substring(0, 1);
+		String LastChar = item.substring(item.length()- 1 , item.length());
+		
+		String SecChar = item.substring(1, 2);
+		String SecLastChar = item.substring(item.length()- 2 , item.length() - 1);
+		
+		
+		name = name.substring(1, name.length()-1);
+		item = item.substring(1, item.length()-1);
+				
+		if (item.equals("null")) {
+			jsonItems.put(name,null);
+		}else if (isNumeric(item)) {
+			int num = Integer.parseInt(item);
+			jsonItems.put(name,num);
 		}
-		else if (next.substring(0,1).equals("[") &&  next.substring(next.length()- 1 , next.length()).equals("]") ) {
-			JsonArrayToCollection ArrayDataObject = new JsonArrayToCollection(next);
-			describeList.addObCollection((Describer) ArrayDataObject);
+		else if (item.equals("true") || item.equals("false")) {
+			Bool booleanobject = new Bool (item);
+			Boolean output = booleanobject.describebool();
+			jsonItems.put(name,output);
+		} 
+		else if (SecChar.equals("[") &&  SecLastChar.equals("]") ) {
+			JsonStringToArray ArrayDataObject = new JsonStringToArray(item);
+			String[] Arr = ArrayDataObject.jsonStringToArray();
+			jsonItems.put(name,Arr);
 		}
-		else if (next.substring(0, 1).equals("\"") &&  next.substring(next.length()- 1 , next.length()).equals("\"")) {
-			
-	    	String testStringOutput = next.substring(1, next.length()-1);
-	    	System.out.println(testStringOutput);
-	    	TextString testStringOutputOut = new TextString(testStringOutput);
-	    	describeList.addObCollection(testStringOutputOut);
-		}else if (isNumeric(next)) {
-				int num = Integer.parseInt(next);
-			 	NumberCast numberOutput = new NumberCast(num);
-			 	describeList.addObCollection(numberOutput);	
-		}else if (next.equals("true") || next.equals("false")) {
-    			Bool booleanobject = new Bool (next);
-				describeList.addObCollection(booleanobject);
-	    }else {
+		else if ((FirstChar.equals("\"") && LastChar.equals("\"") )== true) {
+		    	//String testStringOutput = item.substring(1, item.length()-1);
+		    	//System.out.println(testStringOutput);
+		    	TextString StringOutputOut = new TextString(item);
+		    	String outputString = StringOutputOut.describe();
+		    	jsonItems.put(name,outputString);
+		}
+		else if (item.equals("true") || item.equals("false")) {
+			Bool booleanobject = new Bool (item);
+			Boolean output = booleanobject.describebool();
+			jsonItems.put(name,output);
+		}else {
 	    	throw new Exception("Not Vaild");
 	    }
-		
-		return describeList;
-}
+	
+	  return jsonItems;
+	
+	}
 	public static boolean isNumeric(String str)
 	{
 	  return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
 	}
+	
+	
+			
 }
--- src/test/PolyTests.java
+++ src/test/PolyTests.java
@@ -2,15 +2,14 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 import main.java.uk.ac.uos.i2p.JsonParser.JsonArrayToCollection;
 import main.java.uk.ac.uos.i2p.JsonParser.ObjectParser;
+import main.java.uk.ac.uos.i2p.JsonParser.ItemParser;
 
 
 
@@ -31,34 +30,50 @@ class PolyTests {
 		assertEquals("/task/452359-4435382-6595137 /task/99012-65325148-3574826", FinalCopy);
 	}
 	
-	
-	// ObjectParser Tests
+	// ItemParser Tests
 	@Test
 	void JsonObjectNum55Test() throws Exception {
 		ObjectParser nathan = new ObjectParser("\"Num\":\"55\"");
+		Map<Object, Object> jsonItems= nathan.jsonObject();
+		assertEquals(55, jsonItems.get("Num"));
+	}
+	
+	@Test
+	void JsonObjectDoubleNum55Test() throws Exception {
+		ObjectParser nathan = new ObjectParser("\"Num\":\"55\",\"SecNum\":\"66\"");
+		Map<Object, Object> jsonItems= nathan.jsonObject();
+		assertEquals(55, jsonItems.get("Num"));
+		assertEquals(66, jsonItems.get("SecNum"));
+	}
+	
+	
+	// ItemParser Tests
+	@Test
+	void JsonItemNum55Test() throws Exception {
+		ItemParser nathan = new ItemParser("\"Num\":\"55\"");
 		Map<Object, Object> jsonItems= nathan.jsonItem();
 		assertEquals(55, jsonItems.get("Num"));
 	}
 	@Test
-	void JsonObjectStringTest() throws Exception {
-		ObjectParser nathan = new ObjectParser("\"Where\":\"New New York\"");
+	void JsonItemStringTest() throws Exception {
+		ItemParser nathan = new ItemParser("\"Where\":\"New New York\"");
 		Map<Object, Object> jsonItems= nathan.jsonItem();
 		assertEquals("New New York", jsonItems.get("Where"));
 	}
 	@Test
-	void JsonObjectBoolTest() throws Exception {
-		ObjectParser nathan = new ObjectParser("\"Really\":\"true\"");
+	void JsonItemBoolTest() throws Exception {
+		ItemParser nathan = new ItemParser("\"Really\":\"true\"");
 		Map<Object, Object> jsonItems= nathan.jsonItem();
 		assertEquals(true, jsonItems.get("Really"));
 	}
 	
 	@Test
-	void JsonObjectArrayTest() throws Exception {
-		ObjectParser nathan = new ObjectParser("\"Array\":\"[\\\"/task/452359-4435382-6595137\\\", \\\"/task/99012-65325148-3574826\\\"]\"");
+	void JsonItemArrayTest() throws Exception {
+		ItemParser nathan = new ItemParser("\"GridLock\":\"[\"Car0\",\"Car1\"]\"");
 		Map<Object, Object> jsonItems= nathan.jsonItem();
-		jsonItems.get("Array");
-		
-		assertEquals("/task/452359-4435382-6595137 /task/99012-65325148-3574826", jsonItems.get("Array"));
+		String[] out = (String[]) jsonItems.get("GridLock");
+		assertEquals("Car0", out[0]);
+		assertEquals("Car1", out[1]);
 	}
 	
 	
